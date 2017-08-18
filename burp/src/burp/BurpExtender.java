5064569c37c629bc791a12e38a80dc66c75b879a
Jake Reynolds <jreynoldsdev@gmail.com>
2017-08-18 10:23:39 -0500

    updating to disallow default collaborator

--- burp/src/burp/BurpExtender.java
+++ burp/src/burp/BurpExtender.java
@@ -23,8 +23,8 @@ import java.util.HashMap;
 //Could make a ton of class files pulling this out.
 //In the future if more functionality gets added that should be done,
 // for now this is a manageable size.
-public class BurpExtender implements IBurpExtender, ITab
-{
+public class BurpExtender implements IBurpExtender, ITab {
+
     private IBurpExtenderCallbacks callbacks;
     private IExtensionHelpers helpers;
     private JSplitPane mainPane;
@@ -34,7 +34,7 @@ public class BurpExtender implements IBurpExtender, ITab
     final JCheckBox verboseOutput = new JCheckBox("Verbose");
     private IBurpCollaboratorClientContext collabContext;
     private String burpCollabServerLocation;
-    private HashMap<Integer, String> pollHash = new HashMap<Integer,String>();
+    private HashMap<Integer, String> pollHash = new HashMap<Integer, String>();
     private final String dnsFlag = "nspi";
     private final String amountFlag = "amount";
     private final DNSLogger logger = new DNSLogger();
@@ -44,9 +44,10 @@ public class BurpExtender implements IBurpExtender, ITab
 
     //Log to the receiveTextArea if verbosity is set to true
     class DNSLogger {
+
         public void log(String text) {
             if (verboseOutput.isSelected()) {
-                receiveTextArea.append(text+"\n");
+                receiveTextArea.append(text + "\n");
             }
         }
     }
@@ -59,9 +60,21 @@ public class BurpExtender implements IBurpExtender, ITab
             //Clear any existing data from the pollHash
             pollHash = new HashMap<Integer, String>();
             collabContext = callbacks.createBurpCollaboratorClientContext();
-            String payload = collabContext.generatePayload(true);
-            burpCollabServerLocation = payload;
-            receiveTextArea.setText("Send tunneled data to: " + payload+"\n");
+            //Using the default Burp Collaborator server breaks BurpSuite's Terms and Conditions.
+            if (isServerLocationDefault(collabContext)) {
+                receiveTextArea.setText("You are using the default Burp Collaborator server.  Please specify a private server in Project Options > Misc.");
+                collabContext = null;
+            } else {
+                String payload = collabContext.generatePayload(true);
+                burpCollabServerLocation = payload;
+                receiveTextArea.setText("Send tunneled data to: " + payload + "\n");
+            }
+        }
+        
+        //Check if the current Collaborator server is the default one.  We don't want to allow anyone to use the default, as
+        // it breaks BurpSuite's Terms and Conditions.
+        private boolean isServerLocationDefault(IBurpCollaboratorClientContext collabContext) {
+            return collabContext.getCollaboratorServerLocation().equals("burpcollaborator.net");
         }
     }
 
@@ -71,64 +84,66 @@ public class BurpExtender implements IBurpExtender, ITab
         @Override
         public void actionPerformed(ActionEvent ae) {
             //Get the collaborator interactions
-            List<IBurpCollaboratorInteraction> interactions = collabContext.fetchCollaboratorInteractionsFor(burpCollabServerLocation);
-            String output = "";
-            int tunnelSize = 0;
-            //For every interaction parse the raw packet, add the base32 decoded data to a hashmap,
-            // with each key being the index # of the request.
-            for (int i = 0; i < interactions.size(); i++) {
-                //Get interaction
-                IBurpCollaboratorInteraction interaction = interactions.get(i);
-                Map<String, String> properties = interaction.getProperties();
+            if (collabContext != null) {
+                List<IBurpCollaboratorInteraction> interactions = collabContext.fetchCollaboratorInteractionsFor(burpCollabServerLocation);
+                String output = "";
+                int tunnelSize = 0;
+                //For every interaction parse the raw packet, add the base32 decoded data to a hashmap,
+                // with each key being the index # of the request.
+                for (int i = 0; i < interactions.size(); i++) {
+                    //Get interaction
+                    IBurpCollaboratorInteraction interaction = interactions.get(i);
+                    Map<String, String> properties = interaction.getProperties();
 
-                //Verify interaction is a DNS Address Record (A) query
-                if (properties.get("type").equals("DNS") && properties.get("query_type").equals("A")) {
+                    //Verify interaction is a DNS Address Record (A) query
+                    if (properties.get("type").equals("DNS") && properties.get("query_type").equals("A")) {
 
-                    //Burp base64 encodes the raw queries
-                    byte[] outputBytes = null;
-                    outputBytes = helpers.base64Decode(properties.get("raw_query"));
+                        //Burp base64 encodes the raw queries
+                        byte[] outputBytes = null;
+                        outputBytes = helpers.base64Decode(properties.get("raw_query"));
 
-                    //Parse the raw data and get an array of subdomains
-                    DNSRequestParser parser = new DNSRequestParser();
-                    List<String> outputParser = parser.parseRequestForDomain(outputBytes);
+                        //Parse the raw data and get an array of subdomains
+                        DNSRequestParser parser = new DNSRequestParser();
+                        List<String> outputParser = parser.parseRequestForDomain(outputBytes);
 
-                    //Look for our dnsFlag as the first subdomain and our amountFlag as the second,
-                    // this will tell us how much data to expect
-                    if (outputParser.get(0).equals(dnsFlag) && outputParser.get(1).equals(amountFlag)) {
-                        //Get the tunnel size so we know how much data to expect
-                        tunnelSize = Integer.parseInt(outputParser.get(2));
-                    }
-                    //Look for our dnsFlag as the first subdomain, this will be our data
-                    else if (outputParser.get(0).equals(dnsFlag)) {
-                        logger.log("Chunk " + outputParser.get(2) + " received: " + outputParser.get(1));
-                        pollHash.put(Integer.parseInt(outputParser.get(2)), outputParser.get(1));
+                        //Look for our dnsFlag as the first subdomain and our amountFlag as the second,
+                        // this will tell us how much data to expect
+                        if (outputParser.get(0).equals(dnsFlag) && outputParser.get(1).equals(amountFlag)) {
+                            //Get the tunnel size so we know how much data to expect
+                            tunnelSize = Integer.parseInt(outputParser.get(2));
+                        } //Look for our dnsFlag as the first subdomain, this will be our data
+                        else if (outputParser.get(0).equals(dnsFlag)) {
+                            logger.log("Chunk " + outputParser.get(2) + " received: " + outputParser.get(1));
+                            pollHash.put(Integer.parseInt(outputParser.get(2)), outputParser.get(1));
+                        }
                     }
                 }
-            }
 
-            //Concatenate and base32 decode the tunnel data we've received
-            String outputDecode = "";
-            outputDecode = pollHash.values().stream().map((value) -> value).reduce(outputDecode, String::concat);
-            try {
-                outputDecode = helpers.bytesToString(Base32.decode(outputDecode))+"\n";
-            } catch (Base32.DecodingException ex) {
-                logger.log("Chunks missing, please tunnel data again");
-            }
+                //Concatenate and base32 decode the tunnel data we've received
+                String outputDecode = "";
+                outputDecode = pollHash.values().stream().map((value) -> value).reduce(outputDecode, String::concat);
+                try {
+                    outputDecode = helpers.bytesToString(Base32.decode(outputDecode)) + "\n";
+                } catch (Base32.DecodingException ex) {
+                    logger.log("Chunks missing, please tunnel data again");
+                }
 
-            //Output data about any missing chunks
-            if (pollHash.size() != tunnelSize && tunnelSize>0) {
-                logger.log("Missing " + (pollHash.size() - tunnelSize) + " chunks");
-            } else if (tunnelSize==0) {
-                logger.log("No chunks received");
-            } else {
-                logger.log(pollHash.size() +" chunks received.  All chunks received.");
+                //Output data about any missing chunks
+                if (pollHash.size() != tunnelSize && tunnelSize > 0) {
+                    logger.log("Missing " + (pollHash.size() - tunnelSize) + " chunks");
+                } else if (tunnelSize == 0) {
+                    logger.log("No chunks received");
+                } else {
+                    logger.log(pollHash.size() + " chunks received.  All chunks received.");
+                }
+                receiveTextArea.append("----Data----\n\n" + outputDecode + "------------\n");
             }
-            receiveTextArea.append("----Data----\n\n"+outputDecode+"------------\n");
         }
     }
 
     //Listener for when the "Tunnel data" button is clicked
     class TunnelDataButtonListener implements ActionListener {
+
         @Override
         public void actionPerformed(ActionEvent ae) {
             DataTunneler tunnel = new DataTunneler(burpCollabTextArea.getText(), sendTextArea.getText());
@@ -136,19 +151,20 @@ public class BurpExtender implements IBurpExtender, ITab
                 logger.log("Tunneling data, please wait.");
                 tunnel.tunnelData();
             } catch (UnknownHostException ex) {
-                receiveTextArea.setText("Error tunneling data, please try again.\n"+ex.toString());
+                receiveTextArea.setText("Error tunneling data, please try again.\n" + ex.toString());
             }
         }
     }
 
     //Listener for when the "Tunnel data" button is clicked
     class ClearReceiveButtonListener implements ActionListener {
+
         @Override
         public void actionPerformed(ActionEvent ae) {
             pollHash = new HashMap<Integer, String>();
             //Trigger a new listening session
             //This helps avoid any dns errors
-            for(ActionListener a: startCollabButton.getActionListeners()) {
+            for (ActionListener a : startCollabButton.getActionListeners()) {
                 a.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
             }
         }
@@ -162,26 +178,28 @@ public class BurpExtender implements IBurpExtender, ITab
     //repeat last two lines for each subdomain/hostname/tld
     //0x00 when data is done
     class DNSRequestParser {
+
         DatagramPacket packet;
         byte data[];
         private int off;
         private int len;
         String outputString = "";
         List<String> outputList = new ArrayList<String>();
-        public List<String> parseRequestForDomain (byte[] request) {
+
+        public List<String> parseRequestForDomain(byte[] request) {
             packet = new DatagramPacket(request, request.length);
             this.data = packet.getData();
             this.len = packet.getLength();
             this.off = packet.getOffset();
             //Skip 12 bytes of flags
             //I'm 50% sure it will only ever be 12 bytes...
-            this.off = this.off+12;
-            while(true) {
-                int amount = (int)this.data[this.off];
+            this.off = this.off + 12;
+            while (true) {
+                int amount = (int) this.data[this.off];
                 outputString = "";
                 for (int i = 0; i < amount; i++) {
                     this.off++;
-                    outputString += Character.toString((char)this.data[this.off]);
+                    outputString += Character.toString((char) this.data[this.off]);
                 }
                 outputList.add(outputString);
                 if (this.data[this.off] == 0x00) {
@@ -195,8 +213,10 @@ public class BurpExtender implements IBurpExtender, ITab
 
     //Class for tunneling data through DNS.
     class DataTunneler {
+
         String burpCollabAddress = "";
         String tunnelData = "";
+
         public DataTunneler(String burpCollabAddress, String tunnelData) {
             this.burpCollabAddress = burpCollabAddress;
             this.tunnelData = "";
@@ -206,7 +226,7 @@ public class BurpExtender implements IBurpExtender, ITab
             try {
                 this.tunnelData = Base32.encode(tunnelData.getBytes());
             } catch (Exception ex) {
-                logger.log("Error tunneling data, please try again.\n"+ex.toString());
+                logger.log("Error tunneling data, please try again.\n" + ex.toString());
             }
             if (burpCollabAddress.isEmpty() || tunnelData.isEmpty()) {
                 receiveTextArea.setText("Make sure there is a valid collab address and data to tunnel");
@@ -221,18 +241,18 @@ public class BurpExtender implements IBurpExtender, ITab
             InetAddress.getAllByName(this.burpCollabAddress);
 
             //Had previous issues with caching, I don't think this fixed it though.
-            java.security.Security.setProperty("networkaddress.cache.ttl" , "0");
+            java.security.Security.setProperty("networkaddress.cache.ttl", "0");
 
             //Let the tunnel know the amount of data we are going to send
-            InetAddress.getAllByName(dnsFlag+"."+amountFlag+"."+splitData.size()+"."+this.burpCollabAddress);
+            InetAddress.getAllByName(dnsFlag + "." + amountFlag + "." + splitData.size() + "." + this.burpCollabAddress);
 
             //Thread so we don't hold anything up
             //TODO: Add multiple threads
             Runnable r = () -> {
-                for (int i=0; i < splitData.size() ;i++) {
-                    logger.log("Tunneling chunk " + i + ": "+dnsFlag+"."+splitData.get(i)+"."+i+"."+burpCollabAddress);
+                for (int i = 0; i < splitData.size(); i++) {
+                    logger.log("Tunneling chunk " + i + ": " + dnsFlag + "." + splitData.get(i) + "." + i + "." + burpCollabAddress);
                     try {
-                        InetAddress.getAllByName(dnsFlag+"."+splitData.get(i)+"."+i+"."+burpCollabAddress);
+                        InetAddress.getAllByName(dnsFlag + "." + splitData.get(i) + "." + i + "." + burpCollabAddress);
                     } catch (UnknownHostException ex) {
                         logger.log("Unknown host exception: " + ex.toString());
                     }
@@ -253,8 +273,7 @@ public class BurpExtender implements IBurpExtender, ITab
     }
 
     @Override
-    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks)
-    {
+    public void registerExtenderCallbacks(final IBurpExtenderCallbacks callbacks) {
         // keep a reference to our callbacks object
         this.callbacks = callbacks;
 
@@ -264,11 +283,9 @@ public class BurpExtender implements IBurpExtender, ITab
         callbacks.setExtensionName("DNS Tunnel");
 
         // create our UI
-        SwingUtilities.invokeLater(new Runnable()
-        {
+        SwingUtilities.invokeLater(new Runnable() {
             @Override
-            public void run()
-            {
+            public void run() {
                 //Main split pane
                 mainPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
 
@@ -279,7 +296,7 @@ public class BurpExtender implements IBurpExtender, ITab
 
                 //No newlines in urls
                 burpCollabTextArea.getDocument().putProperty("filterNewlines",
-                Boolean.TRUE);
+                        Boolean.TRUE);
                 burpCollabTextArea.setLineWrap(true);
                 collabAddressWrapper.add(burpCollabTextArea);
 
@@ -341,16 +358,13 @@ public class BurpExtender implements IBurpExtender, ITab
     //
     // implement ITab
     //
-
     @Override
-    public String getTabCaption()
-    {
+    public String getTabCaption() {
         return "DNS Tunnel";
     }
 
     @Override
-    public Component getUiComponent()
-    {
+    public Component getUiComponent() {
         return mainPane;
     }
 }
